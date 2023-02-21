66fb49386ed9c9743eab73e14985b2af888df448
Matthew Nowakowski <nowakowskim98@gmail.com>
2023-02-21 18:50:41 -0500

    Added ColorController feature

---
+++ src/feature/ColorController/code/index.ts
@@ -0,0 +1,42 @@
+import { Board, Led } from "johnny-five";
+import { EtherPortClient } from 'etherport-client';
+
+import Color from './color';
+
+const board = new Board({
+    port: new EtherPortClient({
+        host: '192.168.1.156',
+        port: 3030
+    }),
+    repl: false
+});
+
+const redPin = 4, greenPin = 12, bluePin = 5;
+
+let currentColor: Color = {
+    red: 255,
+    green: 255,
+    blue: 255
+};
+
+const setCurrentColor = (color: Color) => currentColor = color;
+
+board.on('ready', () => {
+    const redLeds = new Led(redPin);
+    const blueLeds = new Led(bluePin);
+    const greenLeds = new Led(greenPin);
+
+    redLeds.on();
+    blueLeds.on();
+    greenLeds.on();
+
+    const colorClamp = 255;
+
+    board.loop(200, () => {
+        redLeds.brightness(currentColor.red % colorClamp);
+        greenLeds.brightness(currentColor.green % colorClamp);
+        blueLeds.brightness(currentColor.blue % colorClamp);
+    });
+});
+
+export { setCurrentColor };
